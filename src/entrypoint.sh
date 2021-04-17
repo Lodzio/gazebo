e5b4b0e236be98868d045b64eec68d53896e83ea
Bartosz Lodek <b.lodek@softwarepartner.pl>
2021-04-17 17:05:58 +0200

    working chomp planner

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,5 +5,5 @@ source /root/ws_moveit/devel/setup.bash
 #roslaunch gazebo_ros empty_world.launch paused:=true use_sim_time:=false gui:=false throttled:=false recording:=false debug:=true verbose:=true
 #rosrun gazebo_ros spawn_model -file /root/ws_moveit/src/my_project/robot.urdf -urdf -x 0 -y 0 -z 1 -model panda
 #gzserver --verbose
-
-roslaunch my_project gazebo.launch
+#roslaunch my_project gazebo.launch
+roslaunch my_project demo_gazebo.launch
